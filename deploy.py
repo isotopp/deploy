ff366a0b81dac55989170bc94898fbd927c7508b
Kristian Koehntopp <kris-git@koehntopp.de>
2023-11-22 22:47:35 +0100

    new operation: code
    we can now deploy code.

--- deploy.py
+++ deploy.py
@@ -1,14 +1,18 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 import argparse
 import getpass
 import json
+import os
 import pwd
+import subprocess
 import sys
 from pathlib import Path
 
 VALID_USERS = ["kris", "joram"]
 VALID_OPERATIONS = ["code", "create", "delete", "logs"]
-PROJECT_DIR = Path("projects")
+PROJECT_DIR = Path("/etc/projects")
+CHECKOUT_CMD = "/usr/bin/git pull --rebase"
+RESTART_CMD = "sudo apachectl stop; sleep 1; sudo apachectl start"
 
 args: argparse.Namespace
 
@@ -48,13 +52,71 @@ def get_opts():
     parser.add_argument("--hostname", help="hostname (in create)")
     parser.add_argument("--unixuser", help="unixuser (in create)")
     parser.add_argument("--projectdir", help="projectdir (in create)")
-
+    parser.add_argument("--checkout-cmd", help="command to check out the source (in create, optional)")
+    parser.add_argument("--restart-cmd", help="command to run after checkout (in create, optional)")
     args = parser.parse_args()
 
 
-def create_deploy_description(project: str, hostname: str, unixuser: str, projectdir: str):
+def show_run_result(result: subprocess.CompletedProcess):
+    code = result.returncode
+    if code == 0:
+        print("*** SUCCESS:")
+        print(result.stdout.decode('utf-8'))
+        print(result.stderr.decode('utf-8'))
+    else:
+        print(f"*** ERROR: exit code={result.returncode}")
+        print(result.stdout.decode('utf-8'))
+        print(result.stderr.decode('utf-8'))
+    print()
+
+
+def deploy_code(project: str):
+    p = PROJECT_DIR / project
+    if not p.is_file():
+        raise ValueError(f"cannot deploy to project {project}, no project file found.")
+
+    config = load_deploy_description(project)
+    os.chdir(config["projectdir"])
+
+    deploy_cmd = f"sudo -u {config['unixuser']} {config['checkout_cmd']}"
+    print(f"Running deploy_cmd {deploy_cmd}")
+
+    deploy_result = subprocess.run(deploy_cmd, shell=True, capture_output=True, timeout=30)
+    show_run_result(deploy_result)
+    if (deploy_result.returncode != 0):
+        print("*** DEPLOYMENT ABORTED ***")
+        print("*** NO SERVICE RESTART ***")
+        return
+
+    restart_cmd = config['restart_cmd']
+    print(f"Running restart_cmd {restart_cmd}")
+
+    restart_result = subprocess.run(restart_cmd, shell=True, capture_output=True, timeout=30)
+    show_run_result(restart_result)
+
+
+def load_deploy_description(project: str) -> dict:
+    p = PROJECT_DIR / project
+    text = p.read_text()
+    data = json.loads(text)
+
+    return data
+
+
+def create_deploy_description(project: str,
+                              hostname: str,
+                              unixuser: str,
+                              projectdir: str,
+                              checkout_cmd: str,
+                              restart_cmd: str):
     if project is None or hostname is None or projectdir is None:
         raise ValueError("Missing options: specify --hostname, --unixuser and --projectdir.")
+
+    if checkout_cmd is None:
+        checkout_cmd = CHECKOUT_CMD
+
+    if restart_cmd is None:
+        restart_cmd = RESTART_CMD
 
     # check the hostname
     if not hostname.endswith("snackbag.net"):
@@ -75,6 +137,8 @@ def create_deploy_description(project: str, hostname: str, unixuser: str, projec
         "hostname": hostname,
         "unixuser": unixuser,
         "projectdir": projectdir,
+        "checkout_cmd": checkout_cmd,
+        "restart_cmd": restart_cmd,
     }
     deployment_json = json.dumps(deployment, indent=2, sort_keys=True) + "\n"
 
@@ -103,10 +167,14 @@ def main():
 
     # operation is one of the things in VALID_OPERATIONS
     if args.operation == "code":
-        pass
+        deploy_code(project=args.project)
     elif args.operation == "create":
-        create_deploy_description(project=args.project, hostname=args.hostname, unixuser=args.unixuser,
-                                  projectdir=args.projectdir)
+        create_deploy_description(project=args.project,
+                                  hostname=args.hostname,
+                                  unixuser=args.unixuser,
+                                  projectdir=args.projectdir,
+                                  checkout_cmd=args.checkout_cmd,
+                                  restart_cmd=args.restart_cmd)
     elif args.operation == "delete":
         delete_deployment_description(project=args.project)
     elif args.operation == "logs":
